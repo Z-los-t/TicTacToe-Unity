fd960a1925d578b987f1a8a3b6e2167d9d4e9561
Z-los-t <144132145+Z-los-t@users.noreply.github.com>
2023-10-25 22:01:26 +0100

    OnClick script

--- TicTacToe/Assets/Scripts/ButtonManager.cs
+++ TicTacToe/Assets/Scripts/ButtonManager.cs
@@ -5,17 +5,14 @@ using UnityEngine.UI;
 public class ButtonManager : MonoBehaviour
 {
    
-   [SerializeField] private  GameObject X;
-    private Button button;
-    private void Start()
+   [SerializeField] private  Sprite X;
+   [SerializeField] private Button[] button;
+   
+    public void Selected(int whichButton)
     {
-        button = GetComponent<Button>();
-    }
-    public void Selected()
-    {
-       var inst= Instantiate(X, button.transform.position, transform.rotation);
-        inst.transform.SetParent(button.transform);
-        button.interactable = false;
-        Debug.Log("geyf");
+        button[whichButton].image.sprite = X;
+        button[whichButton].interactable = false;
+        int index = int.Parse(button[whichButton].name);
+        gameObject.GetComponent<GameManager>().FullButton[whichButton]="X";
     }
 }
